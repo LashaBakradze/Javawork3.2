a6ac925319c0ce182015979f64f5b34949e9a466
Lasha Lasha <lashageorgiy@bk.ru>
2023-03-16 13:47:38 +0300

    Java2

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
 
+        BmiService service = new BmiService();
+        double index = service.calculate(50, 167);
+        int bmi = (int)index;
+
+        System.out.println("Индекс массы тела" + bmi);
+    }
 }
